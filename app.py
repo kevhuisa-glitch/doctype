edbebb1881a1b38fb358aad01c98e53df00db34b
kevhuisa-glitch <kevhuisa@gmail.com>
2026-08-28 15:25:12 -0500

    Añadir botón 'Ingresar' a la interfaz

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ st.title("¡Hola, mundo!")
 
 # Mostrar una animación de globos al cargar la aplicación
 st.balloons()
+
+# Opción "Ingresar"
+if st.button("Ingresar"):
+    st.write("Has presionado 'Ingresar'. ¡Bienvenido!")
+    # Mostrar globos también al presionar "Ingresar"
+    st.balloons()
