4bc8ec712d8a8a5c6201f73aa1a46d7cffbd3d22
kevhuisa-glitch <kevhuisa@gmail.com>
2026-08-28 16:54:20 -0500

    Crear mapamundi interactivo - eliminar código anterior

--- app.py
+++ app.py
@@ -1,55 +1,45 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 import plotly.graph_objects as go
 
-st.title("¡Hola, mundo!")
+st.set_page_config(page_title="Mapamundi Interactivo", page_icon="🌍", layout="centered")
 
-# Mostrar una animación de globos al cargar la aplicación
-st.balloons()
+st.title("🌍 Mapamundi Interactivo")
 
-# Opción "Ingresar"
-if st.button("Ingresar"):
-    st.write("Has presionado 'Ingresar'. ¡Bienvenido!")
-    # Mostrar globos también al presionar "Ingresar"
-    st.balloons()
-
-    # Crear un planeta tierra 3D interactivo con Plotly
-    fig = go.Figure(data=[go.Scattergeo(
-        lon = [-180, -90, 0, 90, 180],
-        lat = [0, 0, 0, 0, 0],
-        mode = 'markers',
-        marker = dict(
-            size = 0,
-            color = 'rgba(0,0,0,0)'
-        )
-    )])
-    
-    fig.update_layout(geo=dict(projection_type='orthographic', showland=True, landcolor='rgb(100, 150, 80)', showocean=True, oceancolor='rgb(30, 90, 150)'))(
-        projection_type = "orthographic",
-        showland = True,
-        landcolor = 'rgb(100, 150, 80)',
-        showocean = True,
-        oceancolor = 'rgb(30, 90, 150)',
-        showlakes = True,
-        lakecolor = 'rgb(50, 120, 180)',
-        showcoastline = True,
-        coastlinecolor = 'rgb(80, 120, 90)',
-        coastlinewidth = 1,
-        countrywidth = 0.5,
-        countrycolor = 'rgb(200, 200, 200)',
-        showframe = False,
-        bgcolor = 'rgba(0, 0, 0, 0.1)'
+# Crear mapamundi 3D interactivo
+fig = go.Figure(data=[go.Scattergeo(
+    lon = [-180, -90, 0, 90, 180],
+    lat = [0, 0, 0, 0, 0],
+    mode = 'markers',
+    marker = dict(
+        size = 0,
+        color = 'rgba(0,0,0,0)'
     )
-    
-    fig.update_layout(
-        title = "🌍 Planeta Tierra 3D",
-        height = 600,
-        margin = dict(l=0, r=0, t=50, b=0),
-        paper_bgcolor = 'rgba(10, 10, 30, 0.9)',
-        font = dict(color = 'white', size = 12)
-    )
-    
-    st.plotly_chart(fig, use_container_width=True)
-    
-    st.success("✨ Visualización interactiva del planeta tierra. ¡Haz clic y arrastra para rotar!")
+)])
+
+fig.update_layout(
+    geo=dict(
+        projection_type='orthographic',
+        showland=True,
+        landcolor='rgb(100, 150, 80)',
+        showocean=True,
+        oceancolor='rgb(30, 90, 150)',
+        showlakes=True,
+        lakecolor='rgb(50, 120, 180)',
+        showcoastline=True,
+        coastlinecolor='rgb(80, 120, 90)',
+        coastlinewidth=1,
+        countrywidth=0.5,
+        countrycolor='rgb(200, 200, 200)',
+        showframe=False,
+        bgcolor='rgba(0, 0, 0, 0.1)'
+    ),
+    title="🌍 Planeta Tierra 3D",
+    height=600,
+    margin=dict(l=0, r=0, t=50, b=0),
+    paper_bgcolor='rgba(10, 10, 30, 0.9)',
+    font=dict(color='white', size=12)
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+st.success("✨ Visualización interactiva del planeta tierra. ¡Haz clic y arrastra para rotar!")
